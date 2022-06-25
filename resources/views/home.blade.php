386d952e65406b5f6cfc454ce5b810d9c253d6af
Il Ya <Jillzay@musica36.com>
2022-06-25 18:34:57 +0300

    delete space

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,8 +7,6 @@
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
-
-                
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
